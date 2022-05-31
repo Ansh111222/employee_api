20567ce1f3c3ce028b148739fd00101cc18ea3a8
Ansh111222 <105754857+Ansh111222@users.noreply.github.com>
2022-06-01 00:05:25 +0530

    employee api

---
+++ README.md
@@ -0,0 +1,17 @@
+## Instructions for Employee API
+
+### Install libraries
+```
+pip install fastapi
+pip install uvicorn
+pip install requests
+pip install sqlalchemy
+```
+
+### Run command
+` uvicorn employee:app --port 8080 --reload `
+
+
+### API Documentation
+
+` http://127.0.0.1:8080/docs `
---
+++ employee.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI, HTTPException, Depends
+from pydantic import BaseModel, Field
+import models
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+class Employee(BaseModel):
+    name: str = Field(min_length=1)
+
+
+@app.get("/employees")
+def read_api(db: Session = Depends(get_db)):
+    return db.query(models.Employee).all()
+
+
+@app.post("/employees")
+def create_employee(employee: Employee, db: Session = Depends(get_db)):
+
+    emp_model = models.Employee()
+    emp_model.name = employee.name
+
+    db.add(emp_model)
+    db.commit()
+
+    return employee
+
+
+@app.get("/employees/{employee_id}")
+def get_employee(employee_id: int, db: Session = Depends(get_db)):
+
+    emp_model = db.query(models.Employee).filter(models.Employee.employee_id == employee_id).first()
+
+    if emp_model is None:
+        raise HTTPException(
+            status_code=404,
+            detail=f"ID {employee_id} : Does not exist"
+        )
+
+    return emp_model
